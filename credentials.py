6d9aecae7182e5f927e55404e831a9ce3c49a472
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-24 01:00:09 +0300

    Add the credentials class and initialise it

---
+++ credentials.py
@@ -0,0 +1,6 @@
+class Credentials:
+  """ Documents the password details"""
+  def __init__(self, page_name, username, password):
+    self.page_name = page_name
+    self.username = username
+    self.password = password 
