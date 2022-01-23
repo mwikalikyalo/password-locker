3e8d64260509315391dfb8b097ab593c23447526
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-24 01:41:40 +0300

    Add the save and delete functions in the credential class

--- credentials.py
+++ credentials.py
@@ -6,3 +6,11 @@ class Credentials:
     self.password = password 
   
   credentials=[]
+
+  def __save_credentials(self):
+    """ Save function for the new credentials"""
+    Credentials.__save_credentials.append(self)
+  
+  def __delete_credentials(self):
+    """ Delete function to delete unnecessary credentials"""
+    Credentials.__delete_credentials.remove(self)
