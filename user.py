eea9813c68c96f45c71ee5bd309e4e171bae9df9
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-24 00:55:52 +0300

    Add the user.py page

---
+++ user.py
@@ -0,0 +1,22 @@
+class User:
+  """ Documents the user login initial details"""
+  user_details=[]
+
+  def __init__(self,full_name,username,password):
+    self.full_name = full_name
+    self.username = username
+    self.password = password
+
+new_user= User(full_name="Winnie Mwikali", 
+              username="mwiks", 
+              password="chichisam")
+
+
+
+def __sign_in__(self,first_name,second_name,email,password):
+    """ Documents how the new user adds their data """
+
+    self.first_name = first_name
+    self.second_name = second_name
+    self.email = email
+    self.password = password
